70cd24454e9127f7935a9578ea99ee6b76a3bf22
youyongsong <youyongsong@gmail.com>
2016-09-05 16:02:26 +0800

    Fix domain name

--- build_context/entrypoint.sh
+++ build_context/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/ngrok/ngrokd -tlsKey=server.key -tlsCrt=server.crt -domain="ysyou.com" -httpAddr=":8081" -httpsAddr=":8082"
+/ngrok/ngrokd -tlsKey=server.key -tlsCrt=server.crt -domain="ysyou.me" -httpAddr=":8081" -httpsAddr=":8082"
